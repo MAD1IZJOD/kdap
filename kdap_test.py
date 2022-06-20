13c1be11dbde7da5bf30d4129de088a255f99e58
descentis <mt4descentis@gmail.com>
2022-06-20 21:55:37 +0530

    updated testing file

--- kdap_test.py
+++ kdap_test.py
@@ -10,14 +10,22 @@ import kdap
 
 knol = kdap.knol()
 
-#knol.download_dataset(sitename='wikipedia', article_list=['Vector', 'Derivative'], destdir='~/knolml_dataset/wikipedia_articles')
+knol.download_dataset(sitename='wikipedia', article_list=['Vector', 'Derivative'], destdir='~/knolml_dataset/wikipedia_articles')
 
-#num_instances = knol.get_num_instances(dir_path='~/knolml_dataset/wikipedia_articles', granularity='yearly', start='2015-01-01')
+num_instances = knol.get_num_instances(dir_path='~/knolml_dataset/wikipedia_articles', granularity='yearly', start='2015-01-01')
 
-#editors = knol.get_editors(dir_path='/home/descentis/knolml_dataset/wikipedia_articles', granularity='monthly')
+print("Displaying the number of instances for articles")
+for key,val in num_instances.items():
+  print(key, val)
+  
+editors = knol.get_editors(dir_path='/home/descentis/knolml_dataset/wikipedia_articles', granularity='monthly')
 
+print("Displaying the editor names")
+for key,val in editors.items():
+  print(key, val)
+  
 #edits = knol.get_author_edits(dir_path='/home/descentis/knolml_dataset/wikipedia_articles', editor_list=['212.219.142.201', 'Redvers', 'Septegram', '87.10.191.84', '72.90.235.102', 'ItsProgrammable'])
 
 #local_gini = knol.get_local_gini_coefficient(dir_path='/home/descentis/knolml_dataset/wikipedia_articles')
 
-adj_list = knol.get_induced_graph_by_articles(['Vector', 'Derivative'])
+#adj_list = knol.get_induced_graph_by_articles(['Vector', 'Derivative'])
